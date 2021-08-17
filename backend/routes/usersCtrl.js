1a2fa3ad70217c185a09312eea41e2d213b5f8a3
Lazo <lourdes.lazo.s@gmail.com>
2021-08-17 23:14:46 +0200

    Ctrl Users

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -15,6 +15,39 @@ module.exports = {
             return res.status(400).json({ 'error': 'missing parameters' });
         }
 
+        models.User.findOne({
+            attributes: ['email'],
+            where: { email: email }
+        })
+            .then(function (userFound) {
+                if (!userFound) {
+
+                    bcrypt.hash(password, 5, function (err, bcryptedPassword) {
+                        var newUser = models.User.create({
+                            email: email,
+                            username: username,
+                            password: bcryptedPassword,
+                            bio: bio,
+                            isAdmin: 0
+                        })
+                            .then(function (newUser) {
+                                return res.status(201).json({
+                                    'userId': newUser.id
+                                })
+                            })
+                            .catch(function (err) {
+                                return res.status(500).json({ 'error': 'cannot add user' });
+                            });
+                    });
+
+                } else {
+                    return res.status(409).json({ 'error': 'user already exist' });
+                }
+            })
+            .catch(function (err) {
+                return res.status(500).json({ 'error': 'unable to verifie user' });
+            });
+
     },
 
     login: function (req, res) {
