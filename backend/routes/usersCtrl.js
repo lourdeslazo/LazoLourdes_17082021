f11997b12e086aa477bcd1609bca1df69f3dd22c
Lazo <lourdes.lazo.s@gmail.com>
2021-08-18 01:02:57 +0200

    Regex email + password

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -2,6 +2,9 @@ var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
 
+//constants
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const PASSWORD_REGEX = /^(?=.*\d).{4,8}$/;
 //Routes
 module.exports = {
     register: function (req, res) {
@@ -13,6 +16,18 @@ module.exports = {
 
         if (email == null || username == null || password == null) {
             return res.status(400).json({ 'error': 'missing parameters' });
+        }
+
+        if (username.length >= 13 || username.length <= 4) {
+            return res.status(400).json({ 'error': 'wrong username (must be lenght 5 - 12)' });
+        }
+
+        if (!EMAIL_REGEX.test(email)) {
+            return res.status(400).json({ 'error': 'email is not valid' });
+        }
+
+        if (!PASSWORD_REGEX.test(password)) {
+            return res.status(400).json({ 'error': 'password invalid (must lenght 4 - 8 and include 1 number at least' });
         }
 
         models.User.findOne({
